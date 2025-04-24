c74ca59bd298beabf073ff07d74f81dd9552a90f
ananastasiap <anestheziia@gmail.com>
2025-04-24 12:05:34 +0300

    feat: настройки реадкс тулкита

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import rootReducer from "./reducers";
+
+const store = configureStore({
+  // reducer: {
+  //   games: gamesReducer,
+  // },
+});
+
+export default store;
